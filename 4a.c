9078617e5cb7a70b0e5828dc8974840f4505fb9f
MD NIZAMUDDIN <102615608+Md-Nizamuddin@users.noreply.github.com>
2023-04-27 10:21:24 +0530

    Add files via upload

---
+++ 4a.c
@@ -0,0 +1,28 @@
+//Q. Output of the program using fork()
+
+#include <stdio.h>
+#include<unistd.h>
+int main()
+    {
+    int a=5,b=10,pid;
+    printf("Before fork a=%d b=%d \n",a,b); 
+    pid=fork();
+    if(pid==0)
+    {
+        a=a+1; b=b+1;
+        printf("In child a=%d b=%d \n",a,b);
+    }
+    else
+        {
+        sleep(1);
+        a=a-1; b=b-1;
+        printf("In Parent a=%d b=%d \n",a,b);
+    }
+return 0;
+}
+
+// --output--
+
+// Before fork a=5 b=10 
+// In child a=6 b=11 
+// In Parent a=4 b=9
