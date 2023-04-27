bcade52203fe1359490288f4e9dc917b48d82b70
MD NIZAMUDDIN <102615608+Md-Nizamuddin@users.noreply.github.com>
2023-04-27 10:36:47 +0530

    Add files via upload
    Week 7
    Pipes
    Question 1
    Implement Unnamed (Anonynmous) Pipe

---
+++ 7a.c
@@ -0,0 +1,33 @@
+//Q. Unnamed(Anonymous) Pipes
+
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+int main()
+{
+    int p[2];
+    char buff[25];
+    pipe(p);
+    if(fork()==0)
+    {
+        printf("Child : Writing to pipe \n");
+        write(p[1],"Welcome",8);
+        printf("Child Exiting\n");
+    }
+    else
+    {
+        wait(NULL);
+        printf("Parent : Reading from pipe \n");
+        read(p[0],buff,8);
+        printf("Pipe content is : %s \n",buff);
+    }
+return 0;
+}
+
+
+// --output--
+
+// Child : Writing to pipe 
+// Child Exiting
+// Parent : Reading from pipe 
+// Pipe content is : Welcome 
