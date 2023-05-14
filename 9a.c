6fc553608bbfca0869ad68d8993ac371f828560b
MD NIZAMUDDIN <102615608+Md-Nizamuddin@users.noreply.github.com>
2023-05-14 23:30:41 +0530

    Update 9a.c

--- 9a.c
+++ 9a.c
@@ -13,10 +13,9 @@ int main()
     char buff[100];  
     int shmid;  
     shmid=shmget((key_t)2345, 1024, 0666|IPC_CREAT);   
-    printf("Key of shared memory is %d\n",shmid);  
     shared_memory=shmat(shmid,NULL,0);   
-    printf("Process attached at %p\n",shared_memory);   
-    printf("Enter some data to write to shared memory\n");  
+    
+    printf("Enter Data \n");  
     read(0,buff,100);
     strcpy(shared_memory,buff);
     printf("You wrote : %s\n",(char *)shared_memory);  
