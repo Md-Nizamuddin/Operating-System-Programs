6989cecdb9c898b7a0ce57282fff48eb0dbeeeac
MD NIZAMUDDIN <102615608+Md-Nizamuddin@users.noreply.github.com>
2023-05-14 12:03:44 +0530

    Update 8b.c
    Week 8
    Message Queue
    Reader Process

--- 8b.c
+++ 8b.c
@@ -4,6 +4,7 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+
 struct mesg_buffer 
 {
 	long mesg_type;
@@ -16,6 +17,7 @@ int main()
 	int msgid;
 	key = ftok("progfile", 65);
 	msgid = msgget(key, 0666 | IPC_CREAT);
+	
 	msgrcv(msgid, &message, sizeof(message), 1, 0);
 	printf("Data Received is : %s \n",message.mesg_text);
 	msgctl(msgid, IPC_RMID, NULL);
